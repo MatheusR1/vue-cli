58b7449d378ecd6e5e9be09cb11e976cc2e73d01
Matheus Rocha <matheusrocha_456@hotmail.com>
2021-07-28 01:04:34 -0300

    adicionando novo compoente ao app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <Head nome='eai boy'/>
+    <lista-filmes
+      v-for='item in filmes'
+      :key='item.id'
+      v-bind='item'
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Head from './components/Head'
+import ListaFilmes from './components/ListaFilmes'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Head,
+    ListaFilmes
+  },
+  data () {
+    return {
+      filmes: [
+        { id: '01', nome: 'A Ameaça Fantasma', ano: 1999 },
+        { id: '02', nome: ' Ataque dos Clones', ano: 2002 },
+        { id: '03', nome: 'Guerras Clônicas', ano: 2008 },
+        { id: '04', nome: 'A Vingança dos Sith ', ano: 2005 }
+      ]
+    }
   }
 }
 </script>
+<style scoped>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
