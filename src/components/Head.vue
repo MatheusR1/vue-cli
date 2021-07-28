bc1874aad36b44c537481c3d055253b919f2baac
Matheus Rocha <matheusrocha_456@hotmail.com>
2021-07-28 01:05:35 -0300

    novo componente para testar passagem de objetos inteiros vue

---
+++ src/components/Head.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>lista filme</h1>
+    <p>Treinando comunicação entre componentes vue</p>
+    <strong> campo passado via props,  nome :{{ nome }} </strong>
+  </div>
+
+</template>
+
+<script>
+export default {
+  props: {
+    nome: String
+  }
+}
+</script>
+
+<style scoped>
+div {
+  background: green
+}
+
+h1 {
+  background: #fff
+}
+</style>
---
+++ src/components/ListaFilmes.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3> Filmes </h3>
+        <ul>
+            <li> {{nome}} | {{ano}} </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    nome: {
+      type: String,
+      require: true
+    },
+    ano: {
+      type: Number,
+      require: true
+    }
+  }
+}
+</script>
